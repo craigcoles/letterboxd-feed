8e39073dc3b614db80df7e293c76dcdbd87cab5b
Craig Coles <craigcoles8@gmail.com>
2013-12-23 23:27:48 +0000

    Changed the stylesheets and started some logic

--- app.rb
+++ app.rb
@@ -2,7 +2,20 @@ require 'bundler'
 Bundler.setup :default
 require 'sinatra'
 
+helpers do
+  # If @title is assigned, add it to the page's title.
+  def title
+    if @title
+      "#{@title}"
+    else
+      "No title"
+    end
+  end
+end
+
 # Set up Homepage
 get '/' do
-  erb :home
+  require 'open-uri'
+  require 'xmlsimple'
+  erb :"pages/home"
 end
