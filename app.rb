bcca538a5d237bc28bd9ebfe282eee310ef2286c
Craig Coles <craigcoles8@gmail.com>
2013-12-27 13:57:13 +0000

    Moved the logic

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
 require 'bundler'
 Bundler.setup :default
 require 'sinatra'
+require 'open-uri'
+require 'xmlsimple'
 
 helpers do
   # If @title is assigned, add it to the page's title.
@@ -15,7 +17,8 @@ end
 
 # Set up Homepage
 get '/' do
-  require 'open-uri'
-  require 'xmlsimple'
+  http = Net::HTTP.new('letterboxd.com')
+  response = http.request(Net::HTTP::Get.new("/craigcoles/rss/"))
+  response_status = XmlSimple.xml_in(response.body, { 'ForceArray' => false })
   erb :"pages/home"
 end
